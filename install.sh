b0096b6bebe8de9f53a8d199189c44b17bcf7fe1
layen67 <lkbcontact@gmail.com>
2019-07-12 20:51:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 domain=$1;
+msqldbpass=$2;
 set -e
 
 git clone https://github.com/layen67/mumaradocker.git;
 sed -i -e "s/yourdomain.com/$1/g" /root/mumaradocker/docker-compose.yml;
+sed -i -e "s/password/$2/g" /root/mumaradocker/docker-compose.yml;
 cd /root/mumaradocker;
 docker-compose up -d;
 sleep 5
 cd /root/mumaradocker/www
 wget https://www.dropbox.com/s/g2nb2nw65yyxz5o/Mumara2.28.tar.gz?dl=0
 tar xzvf Mumara2.28.tar.gz?dl=0
+sed -i -e "s/yourdomain.com/$1/g" /root/mumaradocker/www/inc/db.inc.php;
+sed -i -e "s/password/$2/g" /root/mumaradocker/www/inc/db.inc.php;
