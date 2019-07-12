6fa6553f13c960f0936edb7e0577ebbbbb0c0ed3
layen67 <lkbcontact@gmail.com>
2019-07-12 18:17:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ sed -i -e "s/yourdomain.com/$1/g" /root/mumaradocker/docker-compose.yml;
 git clone https://github.com/layen67/mumaradocker.git;
 cd /root/mumaradocker;
 docker-compose up -d;
+sleep 5
 cd /root/mumaradocker/www
 wget https://www.dropbox.com/s/g2nb2nw65yyxz5o/Mumara2.28.tar.gz?dl=0
+service docker restart
