a166c56c33c661305520fdfd398f2af2dc06054a
layen67 <lkbcontact@gmail.com>
2019-07-12 21:19:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@ sed -i -e "s/yourdomain.com/$1/g" /root/mumaradocker/docker-compose.yml;
 sed -i -e "s/password/$2/g" /root/mumaradocker/docker-compose.yml;
 cd /root/mumaradocker;
 docker-compose up -d;
-sleep 5
-cd /root/mumaradocker/www
-wget https://www.dropbox.com/s/g2nb2nw65yyxz5o/Mumara2.28.tar.gz?dl=0
-tar xzvf Mumara2.28.tar.gz?dl=0
-sed -i -e "s/yourdomain.com/$1/g" /root/mumaradocker/www/inc/db.ini.php;
-sed -i -e "s/password/$2/g" /root/mumaradocker/www/inc/db.ini.php;
+sleep 5;
+cd /root/mumaradocker/www;
+wget https://www.dropbox.com/s/g2nb2nw65yyxz5o/Mumara2.28.tar.gz?dl=0;
+tar xzvf Mumara2.28.tar.gz?dl=0;
+sed -i -e "s/localhost/db/g" /root/mumaradocker/www/inc/db.ini.php;
+sed -i -e "s/Mumara/mumara/g" /root/mumaradocker/www/inc/db.ini.php;
+sed -i -e "s/saas.businessbox.xyz/$1/g" /root/mumaradocker/www/inc/db.ini.php;
+sed -i -e "s/I6R9DmFgCdK4F2/$2/g" /root/mumaradocker/www/inc/db.ini.php;
